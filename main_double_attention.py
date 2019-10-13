52a7ba5d9531eb5b84114d7525496517d8d75f87
Chandralant Bothe <crbothe255@gmail.com>
2019-10-13 22:16:05 +0200

    Training model - full-attention

--- main_double_attention.py
+++ main_double_attention.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.utils import to_categorical
 import pickle, os
@@ -21,6 +20,7 @@ X_Test = padSequencesKeras(X_Test, max_seq_len, toPadding)
 tags, num, Y_train, Y_test = categorize_raw_data(Ztrain, Ztest)
 target_category_test = to_categorical(Y_test, len(tags))
 
+# NON-CONTEXT MODEL
 SINGLE_ATTENTION_VECTOR = False
 model = model_attention_applied_after_bilstm(max_seq_len, 1024, 42, SINGLE_ATTENTION_VECTOR)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -40,14 +40,16 @@ NUM_CLASSES = Y_test_con.shape[1]
 # if True, the attention vector is shared across the input_dimensions where the attention is applied.
 SINGLE_ATTENTION_VECTOR = False
 
+# CONTEXT MODEL
 context_model = context_model_att(seq_length, INPUT_DIM, EMBEDDING_DIM, NUM_CLASSES)
-
 con_model_name = 'params/context_model_att_{}'.format(seq_length)
 callbacks_con = [EarlyStopping(patience=3), ModelCheckpoint(filepath=con_model_name, save_best_only=True)]
 if os.path.exists(con_model_name):
     context_model.load_weights(con_model_name)
+    loss, old_acc = context_model.evaluate(X_test_con, Y_test_con, verbose=2, batch_size=32)
+    print('Context Score results:', old_acc)
 
-train = False
+train = True
 if train == False:
     loss1, new_acc1  = model.evaluate(X_Test, target_category_test, verbose=2)
     print('Non-Context Score results:', new_acc1)
@@ -55,7 +57,7 @@ if train == False:
     print('Context Score results:', new_acc2)
 
 else:
-    old_acc = 0
+    # old_acc = 0
     for iteration in range(10):
         print('Iteration number {}'.format(str(iteration + 1)))
         for i in range(8):
@@ -70,19 +72,20 @@ else:
             Ytarget_out = Ytrain[(i - 1) * len(X_Train):(i - 1) * len(X_Train) + len(X_Train)]
             target_category_train = to_categorical(target, 42)
             print(X_Train.shape, target_category_train.shape)
-            X_Train_con, Y_train_con = preparedata(X_Train, target_category_train, seq_length)
-            print(X_Train_con.shape, Y_train_con.shape)
-
+            X_Train, Y_train_con = preparedata(X_Train, target_category_train, seq_length)
+            print(X_Train.shape, Y_train_con.shape)
+            context_model.load_weights(con_model_name)
             # context_model.train_on_batch(X_Train_con, Y_train_con)
-            context_model.fit(X_Train_con, Y_train_con, epochs=1, batch_size=32, verbose=2,
+            context_model.fit(X_Train, Y_train_con, epochs=1, batch_size=32, verbose=2,
                               callbacks=callbacks_con)  # , validation_split=0.13)
             # con_model_name.load_weights(model_name)
             loss, new_acc = context_model.evaluate(X_test_con, Y_test_con, verbose=2, batch_size=32)
             print('Context Score results:', new_acc)
             if old_acc < new_acc:
                 context_model.save_weights(con_model_name)
-                print('Weights are saved with {} % acc while old acc was {}'.format(new_acc, old_acc))
+                print('Weights are saved with {} % acc while old acc was {} %'.format(new_acc, old_acc))
                 old_acc = new_acc
+            X_Train = 0
 
             # model.fit(X_Train, target_category_train, verbose=2, callbacks=callbacks)
             # model.load_weights('weight_parameters')
