499eb578d2903a8144bc534d6c65d5188ac3220c
Chandrakant Bothe <crbothe255@gmail.com>
2019-10-23 00:40:06 +0300

    Reading dialogues and emotions from iemocap data

--- annotate_iemocap.py
+++ annotate_iemocap.py
@@ -1,27 +1,23 @@
-import os
-from functools import reduce
+from mocap_utils import *
 
-
-def get_directory_structure(rootdir):
-    """
-    Creates a nested dictionary that represents the folder structure of rootdir
-    """
-    dir = {}
-    rootdir = rootdir.rstrip(os.sep)
-    start = rootdir.rfind(os.sep) + 1
-    for path, dirs, files in os.walk(rootdir):
-        folders = path[start:].split(os.sep)
-        subdir = dict.fromkeys(files)
-        parent = reduce(dict.get, folders[:-1], dir)
-        parent[folders[-1]] = subdir
-    return dir
 
 path_tree = get_directory_structure("IEMOCAP")
 
-sessions =['S1', 'S2', 'S3', 'S4', 'S5']
+sessions = ['S1', 'S2', 'S3', 'S4', 'S5']
 
+list_dialogues = []
+file_paths = []
+transcriptions = {}
+emotions = {}
 for session in sessions:
-    for text in path_tree[session]["transcriptions"]:
-        file = open(text)
+    for text in path_tree['IEMOCAP'][session]["transcriptions"]:
+        file_path_utt = "IEMOCAP/" + session + "/transcriptions/" + text
+        file_path_emo = "IEMOCAP/" + session + "/EmoEvaluation/" + text
+        utts = get_transcriptions(file_path_utt)
+        emots = get_emotions(file_path_emo)
+        transcriptions[session + '_' + text] = utts
+        emotions[session + '_' + text] = emots
+        list_dialogues.extend(utts)
+        file_paths.append(file_path_utt)
 
 print('debug')
