af57e4bdde9d77034610e5c4dd5bdcfb1f36a3ab
Chandrakant Bothe <crbothe255@gmail.com>
2019-11-08 02:10:11 +0200

    corrected and plot individual DAs

--- read_evaluate_outputs.py
+++ read_evaluate_outputs.py
@@ -3,23 +3,40 @@ from collections import Counter
 from mocap_annotator import get_mocap_data
 from results.read_predictions_utils import labels_to_indices, read_all_predictions
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 utterances, emotion, emo_evo, v, a, d = get_mocap_data()
 con_elmo_embs, con_diswiz, non_con_elmo_embs, non_con_diswiz = read_all_predictions()
 all_classes = Counter(list(con_diswiz) + list(con_elmo_embs) + list(non_con_diswiz) + list(non_con_elmo_embs))
 classes = list(all_classes.keys())
-
+#  classes = list(Counter(con_elmo_embs).keys())
 dict_of_all_info = {}
 for item in classes:
     v_temp, a_temp = [], []
+    v_a_temp = []
     for i in range(len(con_elmo_embs)):
-        if con_elmo_embs[i] == "fc":
+        if con_elmo_embs[i] == item:
             v_temp.append(v[i])
             a_temp.append(a[i])
-    dict_of_all_info[item] = {"v": v_temp, "a": a_temp, "v_count": Counter(v_temp), "a_count": Counter(a_temp)}
-    for key, value in Counter(a_temp):
-        pass  # TODO plot the different cases
+            v_a_temp.append((v[i], a[i]))
+    v_a_temp_count = Counter(v_a_temp)
+    dict_of_all_info[item] = {"v": v_temp, "a": a_temp, "v_a_count": v_a_temp_count}
+    x, y, z = [], [], []
+    for items in list(v_a_temp_count.keys()):
+        x.append(items[0])
+        y.append(items[1])
+        z.append(v_a_temp_count[items])
+
+    plt.scatter(x, y, np.array(z) * 10, alpha=0.5)
+    plt.title("DA: "+item)
+    plt.ylim(.5, 5.5)
+    plt.xlim(.5, 5.5)
+    plt.xlabel('valence')
+    plt.ylabel('arousal')
+    plt.show()
+    # for key, value in Counter(a_temp):
+    #    pass  # TODO plot the different cases
 
 
 con_elmo_embs = labels_to_indices(con_elmo_embs, classes)
@@ -29,8 +46,5 @@ non_con_diswiz = labels_to_indices(non_con_diswiz, classes)
 
 print("accuracy_score: \n", classification.accuracy_score(con_elmo_embs, non_con_elmo_embs))
 print("matthews_corrcoef: \n", classification.matthews_corrcoef(con_elmo_embs, non_con_elmo_embs))
-
-
-plt.plot()
 
 print('debug')
