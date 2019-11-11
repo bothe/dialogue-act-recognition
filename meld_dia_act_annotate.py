4a1e868bde02bfbaeb5331912d698a0e903908c0
Chandrakant Bothe <bothe@foviatech.com>
2019-11-11 16:39:54 +0100

    function for predicting from features

--- meld_dia_act_annotate.py
+++ meld_dia_act_annotate.py
@@ -1,29 +1,33 @@
-from sklearn.metrics import classification
-from scipy.stats import stats
-from MELD.utils.read_meld import *
-import numpy as np
-from collections import Counter
 import csv
+
+import numpy as np
+from scipy.stats import stats
+from sklearn.metrics import classification
+
+from MELD.utils.read_meld import *
 
 elmo_feature_retrieval = False
 predict_with_elmo = False
 predict_with_elmo_mean = False
+write_final_csv = False
 
 if elmo_feature_retrieval:
     from elmo_features import get_elmo_fea
+
     meld_elmo_features_test = get_elmo_fea(utt_test_data, mean=False)
     np.save('features/meld_elmo_features_test', meld_elmo_features_test)
     meld_elmo_features_dev = get_elmo_fea(utt_dev_data, mean=False)
     np.save('features/meld_elmo_features_dev', meld_elmo_features_dev)
     meld_elmo_features_train = get_elmo_fea(utt_train_data, mean=False)
     np.save('features/meld_elmo_features_train', meld_elmo_features_train)
-else:
+elif predict_with_elmo or predict_with_elmo_mean:
     meld_elmo_features_test = np.load('features/meld_elmo_features_test.npy', allow_pickle=True)
     meld_elmo_features_dev = np.load('features/meld_elmo_features_dev.npy', allow_pickle=True)
     meld_elmo_features_train = np.load('features/meld_elmo_features_train.npy', allow_pickle=True)
 
 if predict_with_elmo:
     from main_predictor_online import predict_classes_from_features
+
     non_con_out, con_out, non_con_out_nums, con_out_nums = predict_classes_from_features(meld_elmo_features_train)
     con_out[0] = non_con_out[0]
     con_out[1] = non_con_out[1]
@@ -39,6 +43,7 @@ if predict_with_elmo_mean:
     meld_elmo_features_train_mean = np.array([item.mean(axis=0) for item in meld_elmo_features_train])
 
     from main_swda_elmo_mean import *
+
     meld_elmo_features_train_mean_con = prepare_data(meld_elmo_features_train_mean, [], seq_length, with_y=False)
     mean_con_predictions = context_model.predict(meld_elmo_features_train_mean_con)
     con_out_mean = []
@@ -61,15 +66,17 @@ print(classification.classification_report(con_out, con_out_mean))
 print('Spearman Correlation between context- and non-context-based prediction: {}'.format(
     stats.spearmanr(con_out, con_out_mean)))
 
-fieldnames = ['speaker', 'uttID', 'utterance', 'emotion',
-              'non_con_out', 'con_out', 'con_out_mean', 'match', 'con_match', 'pre-final-DA', 'all_match']
-store_meld_in_csv = open('results/eda_meld_dataset.csv', mode='w', newline='')
-writer = csv.DictWriter(store_meld_in_csv, fieldnames=fieldnames)
-writer.writeheader()
+if write_final_csv:
+    fieldnames = ['speaker', 'uttID', 'utterance', 'emotion',
+                  'non_con_out', 'con_out', 'con_out_mean', 'match', 'con_match', 'pre-final-DA', 'all_match']
+    store_meld_in_csv = open('results/eda_meld_dataset.csv', mode='w', newline='')
+    writer = csv.DictWriter(store_meld_in_csv, fieldnames=fieldnames)
+    writer.writeheader()
 
 total_match = 0
 con_matches = 0
 any_two_matches = 0
+utt_info_rows = []
 for i in range(len(con_out)):
     match = "NotMatch"
     con_match = "NotConMatch"
@@ -84,19 +91,25 @@ for i in range(len(con_out)):
     if con_out[i] == non_con_out[i] or con_out_mean[i] == con_out[i] or con_out_mean[i] == non_con_out[i]:
         any_two_matches += 1
         match = "Match"
-        if con_out[i] == non_con_out[i]: matched_element = con_out[i]
-        elif con_out_mean[i] == con_out[i]: matched_element = con_out[i]
-        elif con_out_mean[i] == non_con_out[i]: matched_element = con_out_mean[i]
-
-    writer.writerow({'speaker': utt_Speaker_train[i].encode("utf-8"),
-                     'uttID': utt_id_train_data[i],
-                     'utterance': utt_train_data[i].encode("utf-8"),
-                     'emotion': utt_Emotion_train_data[i],
-                     'non_con_out': str(non_con_out[i]), 'con_out': str(con_out[i]),
-                     'con_out_mean': str(con_out_mean[i]), 'pre-final-DA': matched_element,
-                     'match': match, 'con_match': con_match, 'all_match': all_match})
+        if con_out[i] == non_con_out[i]:
+            matched_element = con_out[i]
+        elif con_out_mean[i] == con_out[i]:
+            matched_element = con_out[i]
+        elif con_out_mean[i] == non_con_out[i]:
+            matched_element = con_out_mean[i]
+        utt_info_row = {'speaker': utt_Speaker_train[i].encode("utf-8"),
+                        'uttID': utt_id_train_data[i],
+                        'utterance': utt_train_data[i].encode("utf-8"),
+                        'emotion': utt_Emotion_train_data[i],
+                        'non_con_out': str(non_con_out[i]), 'con_out': str(con_out[i]),
+                        'con_out_mean': str(con_out_mean[i]), 'pre-final-DA': matched_element,
+                        'match': match, 'con_match': con_match, 'all_match': all_match}
+    if write_final_csv:
+        writer.writerow(utt_info_row)
+    utt_info_rows.append(utt_info_row)
 
 print("Matches in all lists(3): {}% and in context lists(2): {}%, any two matches: {}%".format(
-    round((total_match/(i+1))*100, 2), round((con_matches/(i+1))*100, 2), round((any_two_matches/(i+1))*100, 2)))
+    round((total_match / (i + 1)) * 100, 2), round((con_matches / (i + 1)) * 100, 2),
+    round((any_two_matches / (i + 1)) * 100, 2)))
 
 print('debug')
