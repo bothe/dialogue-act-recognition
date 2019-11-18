8e001e3d1137e0cfe92aa0de8f00d685515da18d
Chandralant Bothe <crbothe255@gmail.com>
2019-11-18 22:30:03 +0100

    Donut diagrams final

--- plot_utils.py
+++ plot_utils.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
 
 
-def plot_pie_half_usage(labels, label_values, title, colors, test_show_plot=False):
+def plot_pie_half_usage(labels, label_values, title, colors_emo,
+                        sentiments, sentiments_values, colors_sent, test_show_plot=False):
     # Create a pieplot
     # label_values.append(sum(label_values))
     # labels.append('White')
     print(len(labels), labels)
-    print(len(label_values), label_values)
-    plt.pie(label_values[0:7] + [label_values[-1]], colors=colors, counterclock=False, startangle=180)  # , labels=labels)
+    data = label_values[0:7] + [label_values[-1]]
+    print(len(data), data)
+    n_emo = plt.pie(data, colors=colors_emo, counterclock=False, startangle=180, radius=1.2)
+    for i in range(len(n_emo[0])):
+        n_emo[0][i].set_alpha(0.8)
+
+    data_sent = sentiments_values[0:3] + [sentiments_values[-1]]
+    plt.pie(data_sent, colors=colors_sent, counterclock=False, startangle=180, radius=0.8)
 
     # add a circle at the center
-    my_circle = plt.Circle((0, 0), 0.55, color='white')
+    my_circle = plt.Circle((0, 0), 0.5, color='white')
     p = plt.gcf()
     p.gca().add_artist(my_circle)
 
-    plt.title(title, pad=25)
+    # plt.title(title, y=1)
+    plt.text(0, -0.15, title, fontdict={'size': 15.0, 'horizontalalignment':'center'})
     if test_show_plot:
         plt.show()
         return
@@ -42,7 +50,11 @@ def plot_normal_bars(labels, label_values, title, test_show_plot=False):
 # create data
 # emotions = ['12', '11', '3', '30', '0', '23', '4']
 # values = [12, 11, 3, 30, 0, 23, 4]
+# colors_emo = ['Green', 'Red', 'Orange', 'Blue', 'Magenta', 'Black', 'Gray', 'White']
+# sentiments = ['12', '11', '3']
+# sentiments_values = [12, 11, 3]
+# colors_sent = ['Green', 'Red', 'Orange', 'Blue', 'Magenta', 'Black', 'Gray', 'White']
 # title = 'tag\nHello World'
-#
-# plot_pie_half_usage(emotions, values, title, colors, test_show_plot=True)
+# plot_pie_half_usage(emotions, values, title, colors_emo,
+#                     sentiments, sentiments_values, colors_sent, test_show_plot=True)
 # plot_normal_bars(emotions, values, title, test_show_plot=True)
--- read_annotated_data.py
+++ read_annotated_data.py
@@ -7,21 +7,31 @@ from plot_utils import *
 
 utt, utt_id, utt_Emotion, utt_EDAs, utt_Speaker = read_data_file('results/eda_meld_dataset_bk.csv',
                                                                  annotated_meld=True)
-colors = ['Green', 'Red', 'Orange', 'Gray', 'Magenta', 'Black', 'Blue', 'White']
-emotions = ['joy', 'anger', 'disgust', 'sadness', 'surprise', 'fear', 'neutral']
+train_data = 'MELD/data/MELD/train_sent_emo.csv'
+utt_train_data, dia_id_train_data, utt_id_train_data, \
+utt_Emotion_train_data, utt_Sentiment_train_data, utt_Speaker_train = read_data_file(train_data)
+colors_emo = ['Green', 'Blue', 'Gray', 'Black',   'Magenta', 'Orange', 'Red', 'White']
+emotions = ['joy', 'sadness', 'fear', 'neutral', 'surprise', 'disgust', 'anger', 'White']
+colors_sent = ['Green', 'Black', 'Red', 'White']
+sentiments =['positive', 'neutral', 'negative', 'White']
+
 tags = list(Counter(utt_EDAs).keys())
 pass_emotions, pass_values = [], []
 for tag in tags:
     if tag is not str:
         pass
-    temp_emotion = []
+    temp_emotion, temp_sentiment = [], []
     for i in range(len(utt)):
         if str(utt_EDAs[i]) == str(tag):
             temp_emotion.append(utt_Emotion[i])
-    data = Counter(temp_emotion)
-    values = []
+            temp_sentiment.append(utt_Sentiment_train_data[i])
+    data_emotion = Counter(temp_emotion)
+    data_sentiment = Counter(temp_sentiment)
+    values_emotion, values_sentiment = [], []
     for emotion in emotions:
-        values.append(data[emotion])
+        values_emotion.append(data_emotion[emotion])
+    for sentiment in sentiments:
+        values_sentiment.append(data_sentiment[sentiment])
 
     try:
         title = tag + '\n' + EDAs[tag]
@@ -29,8 +39,10 @@ for tag in tags:
         title = str(tag)
 
     pass_emotions = emotions
-    pass_emotions.extend(['White'])
-    pass_values = values
-    pass_values.extend([sum(values)])
+    pass_values = values_emotion
+    pass_values.extend([sum(values_emotion)])
+
+    pass_values_sent = values_sentiment
+    pass_values_sent.extend([sum(values_sentiment)])
     # plot_normal_bars(emotions, values, title)
-    plot_pie_half_usage(pass_emotions, pass_values, title, colors)
+    plot_pie_half_usage(emotions, pass_values, title, colors_emo, sentiments, pass_values_sent, colors_sent)
