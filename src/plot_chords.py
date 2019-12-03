c4bcd3179a207168cbbde8292443255a37ff746f
Chandrakant Bothe <bothe@foviatech.com>
2019-12-03 17:07:20 +0100

    plotting chords for emotion and DA co-occurrences

---
+++ src/plot_chords.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import holoviews as hv
+from holoviews import opts, dim
+from bokeh.sampledata.les_mis import data
+from bokeh.plotting import show
+
+hv.extension('bokeh')
+hv.output(size=200)
+
+links = pd.DataFrame(data['links'])
+print(links.head(3))
+
+nodes = hv.Dataset(pd.DataFrame(data['nodes']), 'index')
+nodes.data.head()
+
+chord = hv.Chord((links, nodes)).select(value=(5, None))
+chord.opts(
+    opts.Chord(cmap='Category20', edge_cmap='Category20', edge_color=dim('source').str(),
+               labels='name', node_color=dim('index').str()))
+show(hv.render(chord))
