15473c1f52812901cf96244802bd79142bf9d504
Chandrakant Bothe <bothe@foviatech.com>
2020-01-07 17:24:49 +0100

    Corpus annotator in progress ++

--- dia_act_annotator.py
+++ dia_act_annotator.py
@@ -3,6 +3,7 @@ import time
 
 import numpy as np
 import requests
+from flask import Flask, jsonify, request
 from krippendorff import alpha
 from scipy.stats import stats
 from sklearn.metrics import classification
@@ -12,15 +13,15 @@ from main_swda_elmo_predictor import predict_classes_for_elmo
 from src.final_annotator_utils import convert_predictions_to_indices, ensemble_eda_annotation
 from src.utils_float_string import string_to_floats
 
-elmo_feature_retrieval = False
-predict_with_elmo = False
-predict_with_elmo_mean = False
+app = Flask(__name__)
 
 
-def predict_da_classes(speaker_id, utterances, utt_id, emotion, link_online=False):
+@app.route("/elmo_embed_words", methods=['GET', 'POST'])
+def index(speaker_id, utterances, utt_id, emotion, link_online=False):
     """ Predicting from text takes 'x' as a list of utterances and
     will require to have ELMo emb server running at port 4004 or online hosting service. """
 
+    value = request.json['text']
     if os.path.exists('results/tags.npy'):
         tags = np.load('results/tags.npy')
 
@@ -82,11 +83,14 @@ def predict_da_classes(speaker_id, utterances, utt_id, emotion, link_online=Fals
                                                write_final_csv=True, write_utterances=True, return_assessment=True)
 
     print('ran swda_dia_act_annotate.py, with total {} number of utterances'.format(len(rows)))
-    return rows, assessment, k_alpha, fleiss_kappa_score
+    # return rows, assessment, k_alpha, fleiss_kappa_score
+    return jsonify({'result': rows, 'assessment': assessment, 'k_alpha': k_alpha,
+                    'fleiss_kappa_score': fleiss_kappa_score})
 
 
 speaker_id = ["A", "B", "A", "B", "A", "A"]
 utterances = ["I don't know, ", "Where did you go?", "What?", " Where did you go?", "I went to University.", "Uh-huh."]
 utt_id = ["1", "2", " 3", "4", "5", "6"]
 emotions = ["neutral", "surprise", "surprise", "angry", "frustration", "neutral"]
-predict_da_classes(speaker_id, utterances, utt_id, emotions, link_online=True)
+
+index(speaker_id, utterances, utt_id, emotions, link_online=True)
