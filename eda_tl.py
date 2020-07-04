9af7e7b6b81dbf8f73bf563ebac338b62b5fa0e4
Chandrakant Bothe <crbothe@outlook.com>
2020-07-04 11:05:20 +0200

    train-test split added

--- eda_tl.py
+++ eda_tl.py
@@ -11,6 +11,15 @@ from src.utils import categorize_raw_data
 utt_Speaker, utt, utt_Emotion, utt_EDAs = read_data('annotated_data/eda_iemocap_dataset.csv')
 iemocap_elmo_features = np.load('features/iemocap_elmo_features.npy', allow_pickle=True)
 
+test_split = 7869
+utt_ids_train, utt_ids_test = utt_Speaker[:test_split], utt_Speaker[test_split:]
+utt_train, utt_test = utt[:test_split], utt[test_split:]
+utt_Emotion_train, utt_Emotion_test = utt_Emotion[:test_split], utt[test_split:]
+utt_EDAs_train, utt_EDAs_test = utt_EDAs[:test_split], utt_EDAs[test_split:]
+iemocap_elmo_features_train = iemocap_elmo_features[:test_split]
+iemocap_elmo_features_test = iemocap_elmo_features[test_split:]
+
+seq_length = 3
 max_seq_len = 20
 trainFile = 'data/swda-actags_train_speaker.csv'
 testFile = 'data/swda-actags_test_speaker.csv'
@@ -27,8 +36,13 @@ tags, num, Y_train, Y_test = categorize_raw_data(Ztrain, Ztest)
 target_category_test = to_categorical(Y_test, len(tags))
 
 # Preparing test data for contextual training with Seq_length
-seq_length = 3
 X_test_con, Y_test_con = prepare_data(X_Test, target_category_test, seq_length)
+
+iemocap_elmo_features_train = padSequencesKeras(iemocap_elmo_features_train, max_seq_len, toPadding)
+iemocap_elmo_features_train_con = prepare_data(iemocap_elmo_features_train, [], seq_length, with_y=False)
+
+iemocap_elmo_features_test = padSequencesKeras(iemocap_elmo_features_test, max_seq_len, toPadding)
+iemocap_elmo_features_test_con = prepare_data(iemocap_elmo_features_test, [], seq_length, with_y=False)
 
 # NON-CONTEXT MODEL
 model = model_attention_applied_after_bilstm(max_seq_len, X_Test.shape[2], len(tags))
@@ -39,19 +53,18 @@ top_context_model = context_model_att(seq_length, max_seq_len, X_test_con.shape[
 top_con_model_name = 'params/top_context_model_att_{}'.format(seq_length)
 top_context_model.load_weights(top_con_model_name)
 
-iemocap_elmo_features_pad = padSequencesKeras(iemocap_elmo_features, max_seq_len, toPadding)
-iemocap_elmo_features_con = prepare_data(iemocap_elmo_features_pad, [], seq_length, with_y=False)
-
-non_con_predictions = model.predict(iemocap_elmo_features_pad)
-top_con_predictions = top_context_model.predict(iemocap_elmo_features_con)
-
+# CONTEXT MODEL with Pre-trained encoder
 cm_att_with_pt_encoder = context_model_att_with_pt_encoder(seq_length, max_seq_len, X_test_con.shape[3],
                                                            len(tags), model)
 con_model_name = 'params/context_model_3_pt_encoder'
 cm_att_with_pt_encoder.load_weights(con_model_name)
 
-cm_att_with_pt_encoder_preds = cm_att_with_pt_encoder.predict(iemocap_elmo_features_con)
+
+non_con_predictions = model.predict(iemocap_elmo_features_test)
+top_con_predictions = top_context_model.predict(iemocap_elmo_features_test_con)
+cm_att_with_pt_encoder_preds = cm_att_with_pt_encoder.predict(iemocap_elmo_features_test_con)
+
 iemocap_acc = classification.accuracy_score(cm_att_with_pt_encoder_preds.argmax(axis=1),
                                             top_con_predictions.argmax(axis=1))
-
+print('Inter Context Models Accuracy ', iemocap_acc)
 print('debug')
